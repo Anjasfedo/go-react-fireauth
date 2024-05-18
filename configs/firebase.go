0fc19059e0e76a9663a5e9c3556ba029fff74bf2
AnjasFedo <fedoaf98@gmail.com>
2024-05-18 22:55:09 +0700

    Do// Save Middleware

--- configs/firebase.go
+++ configs/firebase.go
@@ -1,4 +1,4 @@
-package firebase
+package configs
 
 import (
 	"context"
@@ -10,17 +10,17 @@ import (
 	"google.golang.org/api/option"
 )
 
-var authClient *auth.Client
+var AuthClient *auth.Client
 
-func initFirebase() {
-	opt := option.WithCredentialsFile("../serviceAccountKey.json")
+func InitFirebase() {
+	opt := option.WithCredentialsFile("serviceAccountKey.json")
 
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
-	authClient, err := app.Auth(context.Background())
+	AuthClient, err = app.Auth(context.Background())
 	if err != nil {
 		log.Fatalf("error getting Auth client: %v\n", err)
 	}
--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	"firebase.google.com/go/auth"
+	"github.com/Anjasfedo/go-react-fireauth/configs"
 )
 
 func main() {
+	configs.InitFirebase()
+
 	r := gin.Default()
 
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
+	r.GET("/api/secure-data", AuthMiddleware(), func(c *gin.Context) {
+		user, exists := c.Get("user")
+		if !exists {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "User data not found in context"})
+			return
+		}
+		
+		userRecord, ok := user.(*auth.UserRecord)
+		if !ok {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to cast user data to *auth.UserRecord"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"userRecord": userRecord,
+		})
+	})
+
 	r.Run()
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uid := c.GetHeader("UID")
+		if uid == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "UID  header required"})
+			c.Abort()
+			return
+		}
+
+		user, err := configs.AuthClient.GetUser(c, uid)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user data"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user", user)
+
+		c.Next()
+	}
 }
