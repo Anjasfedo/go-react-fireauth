ca36f3751680e9f2f201b14f31faacfe3d52f9ec
AnjasFedo <fedoaf98@gmail.com>
2024-05-20 08:02:26 +0700

    Do// Add Cors

--- server/router.go
+++ server/router.go
@@ -39,5 +39,18 @@ func NewRouter() *gin.Engine {
 		}
 	}
 
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	return router
 }
